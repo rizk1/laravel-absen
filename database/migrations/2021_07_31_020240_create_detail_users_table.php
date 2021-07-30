9f980902ff6ba2d20964baca3e0b5af8a3a097ab
rizk1 <48466157+rizk1@users.noreply.github.com>
2021-07-31 02:13:25 +0700

    tabel detail user

---
+++ database/migrations/2021_07_31_020240_create_detail_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_users', function (Blueprint $table) {
+            $table->id();
+            $table->longText('alamat');
+            $table->longText('avatar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_users');
+    }
+}
